3c1d794bee5a366e0a2454955433bc66d3d3e634
Andrea <andyggonzalez@gmail.com>
2026-02-15 21:44:20 -0700

    init: docker ci practica

---
+++ app.py
@@ -0,0 +1,5 @@
+def multiplicar(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print(f"Resultado multiplicar(4,5): {multiplicar(4,5)}")
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from app import multiplicar
+
+def test_multiplicar():
+    assert multiplicar(4, 5) == 20
